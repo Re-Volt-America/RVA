2845caecbc070d923e79cacfc543bc83355a9f5a
José Benavente <jose@bgm.cl>
2023-02-03 15:42:02 -0300

    Add Team <=> User association
    Team belongs_to User

--- app/models/team.rb
+++ app/models/team.rb
@@ -2,6 +2,8 @@ class Team
   include Mongoid::Document
   include Mongoid::Timestamps
 
+  belongs_to :user
+
   field :name, type: String
   field :short_name, type: String
   field :image, type: String
