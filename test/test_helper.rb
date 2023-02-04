4d4ab8655367cbd59c290e6bbd79bf512d1a349a
José Benavente <jose@bgm.cl>
2023-02-04 13:53:27 -0300

    Disable parallelize on Windows
    Having parallelize enabled causes the test suite to produce
    permission errors.

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,10 +1,15 @@
 ENV['RAILS_ENV'] ||= 'test'
 require_relative "../config/environment"
 require "rails/test_help"
+require "etc"
 
 class ActiveSupport::TestCase
   # Run tests in parallel with specified workers
-  parallelize(workers: :number_of_processors, with: :threads)
+
+  # Disable parallelize on Windows as it causes Permission denied errors...
+  unless Etc.uname[:sysname] == "Windows_NT"
+    parallelize(workers: :number_of_processors, with: :threads)
+  end
 
   # Add more helper methods to be used by all tests here...
 end
