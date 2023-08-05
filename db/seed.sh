61563ccc41fc60bf7926e9714e281527a5e960b5
José Benavente <jose@bgm.cl>
2023-08-05 18:26:12 -0400

    Remove back slashes from seed.sh

--- db/seed.sh
+++ db/seed.sh
@@ -1,5 +1,5 @@
-mongoimport --host mongo:27017 --db rv_cars --collection cars --file rv_cars.cars.json --jsonArray && \n
-  mongoimport --host mongo:27017 --db rv_rankings --collection rankings --file rv_rankings.rankings.json --jsonArray && \n
-  mongoimport --host mongo:27017 --db rv_seasons --collection seasons --file rv_seasons.seasons.json --jsonArray && \n
-  mongoimport --host mongo:27017 --db rv_tracks --collection tracks --file rv_tracks.tracks.json --jsonArray && \n
+mongoimport --host mongo:27017 --db rv_cars --collection cars --file rv_cars.cars.json --jsonArray &&
+  mongoimport --host mongo:27017 --db rv_rankings --collection rankings --file rv_rankings.rankings.json --jsonArray &&
+  mongoimport --host mongo:27017 --db rv_seasons --collection seasons --file rv_seasons.seasons.json --jsonArray &&
+  mongoimport --host mongo:27017 --db rv_tracks --collection tracks --file rv_tracks.tracks.json --jsonArray &&
   mongoimport --host mongo:27017 --db rv_users --collection users --file rv_users.users.json --jsonArray
