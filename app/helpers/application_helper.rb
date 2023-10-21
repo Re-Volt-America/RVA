c6df257c7acce42d68bb72232fe5130d600b6855
José Benavente <jose@bgm.cl>
2023-10-21 07:22:04 -0300

    Add extra methods to application_helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -9,12 +9,27 @@ module ApplicationHelper
     user && user.admin?
   end
 
+  # Check if the passed user is a moderator
+  # @return [Boolean] true if the current user is a moderator, false otherwise
+  def user_is_mod?(user = current_user)
+    user && user.mod?
+  end
+
+  # Check if the passed user is an organizer
+  # @return [Boolean] true if the current user is an organizer, false otherwise
+  def user_is_organizer(user = current_user)
+    user && user.organizer?
+  end
+
   # Check if the passed user has a staff role
   # @return [Boolean] true if the current user either an admin, a mod or an organizer
   def user_is_staff?(user = current_user)
     user && (user.admin? || user.mod? || user.organizer?)
   end
 
+  # Check whether the passed object evaluates to a true expression
+  # @param obj [Object]
+  # @return true if the object can be evaluated to "true"
   def true?(obj)
     obj.to_s.downcase == 'true'
   end
