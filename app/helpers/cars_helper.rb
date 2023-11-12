0800ce0d51d5e6244dabb549bb38fbc5c84df807
José Benavente <jose@bgm.cl>
2023-11-12 14:51:14 -0300

    Enhance navigation
    - Added subnav to tracks' index and show views.
    - Added subnav to cars' index and show views.
    - Re-styled several page headers.

--- app/helpers/cars_helper.rb
+++ app/helpers/cars_helper.rb
@@ -1,5 +1,29 @@
 module CarsHelper
+
+  # @return [Array] All the cars of the passed category
   def cars_of_category(category)
     Car.all.filter { |c| c.category == category }
+  end
+
+  # @return [String] Path to the passed category, or nil if not found
+  def car_category_path(category)
+    case category
+    when SYS::CATEGORY::ROOKIE
+      cars_rookie_path
+    when SYS::CATEGORY::AMATEUR
+      cars_amateur_path
+    when SYS::CATEGORY::ADVANCED
+      cars_advanced_path
+    when SYS::CATEGORY::SEMI_PRO
+      cars_semipro_path
+    when SYS::CATEGORY::PRO
+      cars_pro_path
+    when SYS::CATEGORY::SUPER_PRO
+      cars_superpro_path
+    when SYS::CATEGORY::CLOCKWORK
+      cars_clockwork_path
+    else
+      nil
+    end
   end
 end
