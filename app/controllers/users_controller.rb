10753674f85cf05bf97bf63a0aa571a28c71e0bb
José Benavente <jose@bgm.cl>
2023-11-13 11:56:44 -0300

    Stay in users import page in case of errors

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -24,7 +24,7 @@ class UsersController < ApplicationController
     file = params[:file]
     if file.nil?
       respond_to do |format|
-        format.html { redirect_to root_path, :notice => 'You must select a CSV file.' }
+        format.html { redirect_to users_new_path, :notice => 'You must select a CSV file.' }
         format.json { render :json => 'You must select a CSV file.', :status => :bad_request, :layout => false }
       end
 
@@ -33,7 +33,7 @@ class UsersController < ApplicationController
 
     if file.content_type != SYS::CSV_TYPE
       respond_to do |format|
-        format.html { redirect_to root_path, :note => 'You may only upload CSV files.' }
+        format.html { redirect_to users_new_path, :note => 'You may only upload CSV files.' }
         format.json { render :json => 'You may only upload CSV files.', :status => :bad_request, :layout => false }
       end
 
