4dcf16beb451df978080f5762fbc800b6e33735e
José Benavente <jose@bgm.cl>
2023-11-16 23:06:13 -0300

    Add rankings' leaderboards

--- app/controllers/rankings_controller.rb
+++ app/controllers/rankings_controller.rb
@@ -9,6 +9,8 @@ class RankingsController < ApplicationController
   def show
     @sessions = @ranking.sessions
     @sessions = Kaminari.paginate_array(@sessions).page(params[:page]).per(8)
+    @count = 1
+    @entries = @ranking.racer_result_entries
 
     respond_with @ranking do |format|
       format.json { render :layout => false }
