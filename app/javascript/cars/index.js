2e5a62b992f3c036954cac9d40da4703eab403de
José Benavente <jose@bgm.cl>
2023-11-17 23:13:40 -0300

    Fix car search
    Resolves #16

--- app/javascript/cars/index.js
+++ app/javascript/cars/index.js
@@ -1,7 +1,7 @@
 $(document).on('turbo:load', function () {
     $("#input").on("keyup", function() {
-        const value = $(this).val().toLowerCase();
-        $("#car-card").filter(function() {
+        var value = $(this).val().toLowerCase();
+        $(".car-card").filter(function() {
             $(this).toggle($(this).text().toLowerCase().indexOf(value) > -1)
         });
     });
