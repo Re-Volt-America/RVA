3bece35f3da2236c3a2737f960fa1dae1f02e6b0
José Benavente <jose@bgm.cl>
2023-11-20 19:53:11 -0300

    Use Shrine's memory storage in test environment

--- config/initializers/shrine.rb
+++ config/initializers/shrine.rb
@@ -1,10 +1,19 @@
 require 'shrine'
 require 'shrine/storage/file_system'
+require 'shrine/storage/memory'
+require 'shrine/storage/s3'
 
-Shrine.storages = {
-  :cache => Shrine::Storage::FileSystem.new('public', :prefix => 'uploads/cache'), # temporary
-  :store => Shrine::Storage::FileSystem.new('public', :prefix => 'uploads') # permanent
-}
+if Rails.env.test?
+  Shrine.storages = {
+    :cache => Shrine::Storage::Memory.new, # temporary
+    :store => Shrine::Storage::Memory.new # permanent
+  }
+else
+  Shrine.storages = {
+    :cache => Shrine::Storage::FileSystem.new('public', :prefix => 'uploads/cache'), # temporary
+    :store => Shrine::Storage::FileSystem.new('public', :prefix => 'uploads') # permanent
+  }
+end
 
 Shrine.plugin :mongoid, :validations => false
 Shrine.plugin :cached_attachment_data # enables retaining cached file across form redisplays
