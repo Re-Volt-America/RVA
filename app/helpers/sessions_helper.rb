273868b3ab90bd55b7bce2c7aea570c0fdaba20a
José Benavente <jose@bgm.cl>
2023-11-21 20:25:54 -0300

    Add metadata to sessions
    Progress towards #18

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -6,4 +6,28 @@ module SessionsHelper
 
     current_ranking.sessions.last
   end
+
+  def singles_session_meta_desc(rva_results)
+    top1 = rva_results[1][3].is_a?(User) ? rva_results[1][3].username : rva_results[1][3]
+    top2 = rva_results[3][3].is_a?(User) ? rva_results[3][3].username : rva_results[3][3]
+    top3 = rva_results[5][3].is_a?(User) ? rva_results[5][3].username : rva_results[5][3]
+
+    "#{pretty_datetime(rva_results[1][1])}\n
+    Top Score:
+        1. #{top1} (#{rva_results[1][9]})
+        2. #{top2} (#{rva_results[3][9]})
+        3. #{top3} (#{rva_results[5][9]})"
+  end
+
+  def teams_session_meta_desc(rva_results)
+    top1 = rva_results[1][3].is_a?(User) ? rva_results[1][3].username : rva_results[1][3]
+    top2 = rva_results[3][3].is_a?(User) ? rva_results[3][3].username : rva_results[3][3]
+    top3 = rva_results[5][3].is_a?(User) ? rva_results[5][3].username : rva_results[5][3]
+
+    "#{pretty_datetime(rva_results[1][1])}\n
+    Top Score:
+        1. #{top1} (#{rva_results[1][7]})
+        2. #{top2} (#{rva_results[3][7]})
+        3. #{top3} (#{rva_results[5][7]})"
+  end
 end
