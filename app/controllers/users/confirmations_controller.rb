f036136226d265a7c5f4fdccebea49bf3085508b
José Benavente <jose@bgm.cl>
2023-11-21 20:47:57 -0300

    Fix wrong redirection after resending user confirmation instructions
    Resolves #34

--- app/controllers/users/confirmations_controller.rb
+++ app/controllers/users/confirmations_controller.rb
@@ -20,9 +20,9 @@ module Users
     # protected
 
     # The path used after resending confirmation instructions.
-    # def after_resending_confirmation_instructions_path_for(resource_name)
-    #   super(resource_name)
-    # end
+    def after_resending_confirmation_instructions_path_for(resource_name)
+      root_path
+    end
 
     # The path used after confirmation.
     def after_confirmation_path_for(_resource_name, resource)
