3aff83fe64a238e3f74775f5e154caf20f4ed1d2
José Benavente <jose@bgm.cl>
2023-11-27 12:39:03 -0300

    Add redirects for well known alternative nicknames

--- config/routes/user.rb
+++ config/routes/user.rb
@@ -1,6 +1,11 @@
 RVA::Application.routes.draw do
   get '/users', :to => redirect('/users/register')
 
+  # Well known alternative nicknames get their own redirects
+  get '/B', :to => redirect('/BGM')
+  get '/G', :to => redirect('/GFORCE')
+  get '/V', :to => redirect('/VENDETT5')
+
   get '/users/new', :to => 'users#new'
   post '/users/import', :to => 'users#import'
 
