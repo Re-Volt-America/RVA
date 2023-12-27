fc0b2f9ddac750b49e41d3ee7b86f765900d4367
José Benavente <jose@bgm.cl>
2023-12-27 02:34:56 -0300

    Update cars' import service
    When an imported car already exists in a season, the cars import service will now try to update
    its multiplier in case it has been changed.

--- app/services/csv_import_cars_service.rb
+++ app/services/csv_import_cars_service.rb
@@ -13,21 +13,28 @@ class CsvImportCarsService
     csv = CSV.parse(File.open(@file))
 
     cars = []
-    csv.drop(1).each do |car|
-      # Don't create duplicate cars with the same name and season
-      match = Car.find { |c| c.name.eql?(car[0]) && c.season_id.to_s.eql?(@season) }
-      next unless match.nil?
+    csv.drop(1).each do |row|
+      next if row.empty?
+      next if row.include?(nil)
+
+      # If the car already exists in this season, then only reassign its multiplier
+      match = Car.find { |c| c.name.eql?(row[0]) && c.season_id.to_s.eql?(@season) }
+      unless match.nil?
+        match.update_attribute(:multiplier, row[4].to_f)
+        cars << match
+        next
+      end
 
       car_hash = {
         :season => @season,
         :category => @category,
-        :name => car[0],
-        :speed => car[1],
-        :accel => car[2],
-        :weight => car[3],
-        :multiplier => car[4],
-        :folder_name => car[5],
-        :stock => true?(car[6])
+        :name => row[0],
+        :speed => row[1],
+        :accel => row[2],
+        :weight => row[3],
+        :multiplier => row[4],
+        :folder_name => row[5],
+        :stock => true?(row[6])
       }
 
       cars << Car.new(car_hash)
