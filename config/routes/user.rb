168cb1939c92adf0298623c67e8229cd892d29c9
José Benavente <jose@bgm.cl>
2024-01-13 20:16:27 -0300

    Update redirects

--- config/routes/user.rb
+++ config/routes/user.rb
@@ -1,10 +1,15 @@
 RVA::Application.routes.draw do
   get '/users', :to => redirect('/users/register')
 
-  # Well known alternative nicknames get their own redirects
+  # Well known users with alternative nicknames get their own redirects
   get '/B', :to => redirect('/BGM')
+  get '/b', :to => redirect('/BGM')
+
   get '/G', :to => redirect('/GFORCE')
+  get '/g', :to => redirect('/GFORCE')
+
   get '/V', :to => redirect('/VENDETT5')
+  get '/v', :to => redirect('/VENDETT5')
 
   get '/users/new', :to => 'users#new'
   post '/users/import', :to => 'users#import'
