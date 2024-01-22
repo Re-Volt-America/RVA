a32676c0c3ecfa06cd1db50e4f73667ad0d513da
José Benavente <jose@bgm.cl>
2024-01-22 10:02:32 -0300

    Fix typo

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,3 +1,3 @@
 # frozen_string_literal: true
 
-server 'server02.rva.lat', :user => 'deploy', :roles => %[web app], :primary => true
+server 'server02.rva.lat', :user => 'deploy', :roles => %w[web app], :primary => true
