27873278e879f8bbc59db098463c05a915f574f5
José Benavente <jose@bgm.cl>
2024-03-09 17:54:26 -0300

    Allow organizers to upload sessions
    Progress towards #61

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,6 +1,6 @@
 class SessionsController < ApplicationController
   before_action :authenticate_user!, :except => [:index, :show]
-  before_action :authenticate_staff, :except => [:index, :show]
+  before_action :authenticate_organizer, :except => [:index, :show]
   before_action :set_session, :only => [:show, :edit, :update, :destroy]
 
   respond_to :html, :json
