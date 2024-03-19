5f9ce0dcf9d9ba3086a6315c111a0df3c5cf4932
José Benavente <jose@bgm.cl>
2024-03-19 15:13:06 -0300

    Allow guest users to select their locale
    This commit makes it so that guest users can select their locale
    from the locale selector as if they were logged in.
    
    Moreover, guest users' locale on the site will now be automatically
    determined based on their browser settings in case they haven't
    selected one themselves.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,11 +6,20 @@ class ApplicationController < ActionController::Base
   before_action :build_navigation
 
   def set_locale
-    I18n.locale = if user_signed_in?
-                    current_user.locale
-                  else
-                    I18n.default_locale
-                  end
+    if user_signed_in?
+      I18n.locale = current_user.locale
+    else
+      I18n.locale = params[:locale] || locale_from_header || I18n.default_locale
+    end
+  end
+
+  def default_url_options
+    { :locale => I18n.locale }
+  end
+
+  # Detect Browser's locale and use that by default
+  def locale_from_header
+    request.env.fetch('HTTP_ACCEPT_LANGUAGE', '').scan(/[a-z]{2}/).first
   end
 
   def build_navigation
