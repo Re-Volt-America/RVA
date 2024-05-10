963f8ee96820dd2713307b07e3d767017d363a50
José Benavente <jose@bgm.cl>
2024-05-10 14:52:23 -0400

    Small layout updates

--- app/helpers/weekly_schedules_helper.rb
+++ app/helpers/weekly_schedules_helper.rb
@@ -1,2 +1,6 @@
 module WeeklySchedulesHelper
+  # @return The most recent weekly schedule, or nil if there are no schedules in the system
+  def current_schedule
+    WeeklySchedule.all.max_by { |s| s[:start_date] }
+  end
 end
