832f4c7c456d7d49786ad3ef8cd953073ec99171
José Benavente <jose@bgm.cl>
2024-05-10 15:29:56 -0400

    Update WeeklySchedule model permissions

--- app/controllers/weekly_schedules_controller.rb
+++ app/controllers/weekly_schedules_controller.rb
@@ -1,6 +1,6 @@
 class WeeklySchedulesController < ApplicationController
   before_action :authenticate_user!, :except => [:index, :show]
-  before_action :authenticate_organizer, :except => [:index, :show]
+  before_action :authenticate_staff, :except => [:index, :show]
   before_action :set_weekly_schedule, :only => [:show, :edit, :update, :destroy]
 
   respond_to :html, :json
