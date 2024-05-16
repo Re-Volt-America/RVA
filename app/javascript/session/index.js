c1c9333324db26648d42b7a36b6208921681ff27
nickskyline <nicolas@nickskyline.dev>
2024-05-16 10:05:04 -0500

    Replace getElementById with jQuery for screenshot handling

--- app/javascript/session/index.js
+++ app/javascript/session/index.js
@@ -1,16 +1,16 @@
 import html2canvas from "html2canvas";
 
 $(document).on('turbo:load', function () {
-    document.getElementById(`dl-png`).onclick = function () {
-        const screenshotTarget = document.getElementById(`session`);
+    $(`#dl-png`).on('click', function () {
+        const screenshotTarget = $(`#session`);
 
-        html2canvas(screenshotTarget).then((canvas) => {
+        html2canvas(screenshotTarget[0]).then((canvas) => {
             const base64image = canvas.toDataURL("image/png");
             var anchor = document.createElement('a');
             anchor.setAttribute("href", base64image);
             anchor.setAttribute("download", `session.png`);
             anchor.click();
             anchor.remove();
-        })
-    }
-})
+        });
+    });
+});
