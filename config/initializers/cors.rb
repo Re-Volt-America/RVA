21c3992b3fe372e5dd0cee780587e752ef69970f
José Benavente <jose@bgm.cl>
2025-05-04 16:47:26 -0400

    Update cors

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -10,7 +10,7 @@ Rails.application.config.middleware.insert_before 0, Rack::Cors do
     if Rails.env.development?
       origins 'localhost:3000', 'localhost:7000', 'rva.lat'
     else
-      origins 'rva.lat'
+      origins 'rva.lat', 'staging.rva.lat'
     end
 
     resource '*',
