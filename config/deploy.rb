f1ee549500d21764900e359a71a16a690c5f5ef5
José Benavente <jose@bgm.cl>
2025-05-14 12:13:59 -0400

    Update capistrano linked dirs

--- config/deploy.rb
+++ config/deploy.rb
@@ -8,11 +8,17 @@ set :branch, 'production'
 set :user, 'rva'
 set :stages, %w(production)
 set :deploy_to, '/home/rva/RVA'
-set :linked_dirs, %w(log tmp/pids tmp/cache tmp/sockets vendor/bundle .bundle public/system public/uploads public/uploads/cache)
+set :linked_dirs, %w(log tmp/pids tmp/cache tmp/sockets vendor/bundle .bundle public/system public/uploads)
 set :linked_files, %w(.env)
 set :pty, true
 
 namespace :app do
+  task :set_permissions do
+    on roles(:app) do
+      execute :sudo, "chmod -R 777 #{shared_path}"
+    end
+  end
+
   task :restart do
     on roles(:app) do
       execute :sudo, '/bin/systemctl restart rva.service'
